5cac7f88c6c0b35259fb4319bf42090c17233ca6
Graham Allen <grahamtallen94@gmail.com>
2019-10-26 16:45:57 -0500

    uses global position instead of relative position to get value of onClick

--- index.js
+++ index.js
@@ -38,7 +38,9 @@ class Window {
 	}
 
 	onClick(clickedX = 0, clickedY = 0, hideLog = false) {
-		const {x, y, w, h} = this;
+		const {w, h} = this;
+		const {x, y} = this.getGlobalPosition();
+		console.log(x, y)
 		const withinXBounds = (x <= clickedX && clickedX <= (x + w));
 		const withinYBounds = (y <= clickedY && clickedY <= (y + h));
 		let windowHasBeenClicked = withinXBounds && withinYBounds;
@@ -46,7 +48,7 @@ class Window {
 		if (windowHasBeenClicked) {
 			!hideLog && this.logClicked(this);
 			for (const childWindow of this.children.values()) {
-				childWindow.onClick(clickedX - x, clickedY - x, hideLog)
+				childWindow.onClick(clickedX, clickedY, hideLog)
 			}
 
 		}
@@ -57,27 +59,46 @@ class Window {
 		console.log(win.id, " clicked")
 	}
 
-	printReport() {
+	printReport(nestingLevel = 0) {
 		const {id, x, y, w, h, parent, children} = this;
 		const isRoot = !parent;
-		console.log(`${!isRoot ? " - " : ""}${id} ${x} ${y} ${w} ${h}\n`);
+		let tabLevel = "";
+		for (let i = 0; i <= nestingLevel; i++) {
+			tabLevel = tabLevel + "\t";
+		}
+		console.log(`${tabLevel}${!isRoot ? " - " : ""}${id} ${x} ${y} ${w} ${h}\n`);
 		for (const childWindow of this.children.values()) {
-			childWindow.printReport();
+			childWindow.printReport(nestingLevel + 1);
 		}
 	}
 }
 
 const root = new Window("root", 0, 0, 1024, 768);
-const child1 = root.addChild(new Window("child1", 20, 20, 100, 200))
-const child2 = root.addChild(new Window("child2", 25, 25, 100, 200))
-const child3 = root.addChild(new Window("child3", 30, 30, 400, 400))
-const child4 = child3.addChild(new Window("child4", 30, 30, 400, 400))
+const child1 = root.addChild(new Window("child1", 20, 20, 100, 200));
+const child2 = root.addChild(new Window("child2", 25, 25, 100, 200));
+const child3 = root.addChild(new Window("child3", 30, 30, 400, 400));
+const child4 = child3.addChild(new Window("child4", 2, 0, 400, 10));
+const button1 = child3.addChild(new Window("button1", 0, 0, 10, 10));
+const button2 = child3.addChild(new Window("button2", 10, 0, 10, 10));
+const button3 = child3.addChild(new Window("button3", 20, 0, 10, 10));
+const button4 = child3.addChild(new Window("button4", 30, 0, 10, 10));
+const child5 = root.addChild(new Window("child5", 40, 20, 400, 400));
+const child6 = child5.addChild(new Window("child6", 2, 0, 400, 10));
+const button5 = child5.addChild(new Window("button5", 0, 0, 10, 10));
+const button6 = child5.addChild(new Window("button6", 10, 0, 10, 10));
+const button7 = child5.addChild(new Window("button7", 20, 0, 10, 10));
+const button8 = child5.addChild(new Window("button7", 30, 0, 10, 10));
 
 
 root.printReport()
+root.onClick(45, 25)
 
 // tests
 
+// passing
 assert(root.onClick(1024, 768, true));
 assert(!root.onClick(1025, 768, true));
 assert(!root.onClick(1025, 769, true));
+
+// failing
+assert(child5.onClick(5, 5))
