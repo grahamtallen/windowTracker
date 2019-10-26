00f46ae7aff14b5cfbc8f122144b443f8bd7fa3f
Graham Allen <grahamtallen94@gmail.com>
2019-10-26 16:06:48 -0500

    onClick working for root

--- index.js
+++ index.js
@@ -22,9 +22,42 @@ class Window {
 	addParent(parentWindow) {
 		this.parent = parentWindow;
 	}
+
+	getGlobalPosition(x = 0, y = 0) {
+		if (!this.parent) {
+			return {
+				x: x + this.x, 
+				y: y + this.y
+			}
+		} else {
+			return this.parent.getGlobalPosition(x + this.x, y + this.y)
+		}
+	}
+
+	getLocalPosition() {
+		return {x: this.x, y: this.y}
+	}
+
+	onClick(clickedX, clickedY) {
+		const {x, y, w, h} = this;
+		const withinXBounds = (x <= clickedX && clickedX <= (x + w));
+		const withinYBounds = (y <= clickedY && clickedY <= (y + h));
+		if (withinXBounds && withinYBounds) {
+			this.logClicked(this)
+		}
+	}
+
+	logClicked(win) {
+		console.log(win.id, " clicked")
+	}
 }
 
 const root = new Window("root", 0, 0, 1024, 768);
 const child1 = root.addChild(new Window("child1", 20, 20, 100, 200))
 
-console.log(root.children.get("child1").parent.children.get("child1"))
+
+
+
+// tests
+
+root.onClick(1024, 768); // true
