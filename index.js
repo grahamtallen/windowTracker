a46aa7a5d2701c6fbe0c9406d3d8f51f61368b23
Graham Allen <grahamtallen94@gmail.com>
2019-10-26 16:23:53 -0500

    working printReport

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+const assert = require("assert");
+
 class Window {
-
-
 
 	constructor(id, x, y, w, h) {
 		this.id = id;
@@ -10,13 +10,12 @@ class Window {
 		this.h = h;
 		this.children = new Map();
 		this.parent = null;
-
-
 	}
 
 	addChild(window) {
 		this.children.set(window.id, window);
 		window.addParent(this);
+		return window;
 	}
 
 	addParent(parentWindow) {
@@ -38,26 +37,47 @@ class Window {
 		return {x: this.x, y: this.y}
 	}
 
-	onClick(clickedX, clickedY) {
+	onClick(clickedX = 0, clickedY = 0, hideLog = false) {
 		const {x, y, w, h} = this;
 		const withinXBounds = (x <= clickedX && clickedX <= (x + w));
 		const withinYBounds = (y <= clickedY && clickedY <= (y + h));
-		if (withinXBounds && withinYBounds) {
-			this.logClicked(this)
+		let windowHasBeenClicked = withinXBounds && withinYBounds;
+
+		if (windowHasBeenClicked) {
+			!hideLog && this.logClicked(this);
+			for (const childWindow of this.children.values()) {
+				childWindow.onClick(clickedX - x, clickedY - x, hideLog)
+			}
+
 		}
+		return windowHasBeenClicked;
 	}
 
 	logClicked(win) {
 		console.log(win.id, " clicked")
 	}
+
+	printReport() {
+		const {id, x, y, w, h, parent, children} = this;
+		const isRoot = !parent;
+		console.log(`${!isRoot ? " - " : ""}${id} ${x} ${y} ${w} ${h}\n`);
+		for (const childWindow of this.children.values()) {
+			childWindow.printReport();
+		}
+	}
 }
 
 const root = new Window("root", 0, 0, 1024, 768);
 const child1 = root.addChild(new Window("child1", 20, 20, 100, 200))
+const child2 = root.addChild(new Window("child2", 25, 25, 100, 200))
+const child3 = root.addChild(new Window("child3", 30, 30, 400, 400))
+const child4 = child3.addChild(new Window("child4", 30, 30, 400, 400))
 
 
-
+root.printReport()
 
 // tests
 
-root.onClick(1024, 768); // true
+assert(root.onClick(1024, 768, true));
+assert(!root.onClick(1025, 768, true));
+assert(!root.onClick(1025, 769, true));
